0771d6b3f009744090abc92594b6d1e60ea5153a
DanielG0m <156710527+DanielG0m@users.noreply.github.com>
2024-01-30 21:44:03 +0200

    feat: :tada: Modulos
    Creacion, edicion y manipulacion de modulos

---
+++ modulate/camper.py
@@ -0,0 +1,11 @@
+def create():
+    print("Camper se guardo ")
+
+def read():
+    print("Datos del camper ")
+
+def update():
+    print("Camper actualizado ")
+
+def delete():
+    print("Camper eliminado")
