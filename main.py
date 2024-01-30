58dbb0d929e543205259f2e5d6d02447b6dd67eb
DanielG0m <156710527+DanielG0m@users.noreply.github.com>
2024-01-30 22:53:17 +0200

    refactor: :construction:

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import modulate.camper as camper
 
-camper.menu()
+#camper.menu()
+
+# data= ["Hola", "Como esta", "Bien rey"]
+
+# for i,val in enumerate(sorted(data)):
+#     data[i]=val
+#     print(f"{i+1}. {val}")
+    #print(f"{data.index(i)+1}. {i}")
+
+# data=[f"{i+1}. {val}" for i,val in enumerate(sorted(["a", "Miguel", "BEBE"]))]
+# opc= int(input())
+# print(data[opc-1])
+
+#Index(i) enumerar los elementos de las listas sin creacion de las variables
+#iterar y alterar lista principal
+
+#ternarios
+# hola= [f"Data {i}" for i in ["a", "Miguel", "BEBE"]]
+
+# hola=[]
+# for i in ["a", "Miguel", "BEBE"]:
+# hola.append(f"Data {i}")
+
+# print(hola)
